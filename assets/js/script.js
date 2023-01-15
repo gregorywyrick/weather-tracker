13116fb750a82ef3335f7d208cb40d054012e29f
gregorywyrick <gregmwyrick@gmail.com>
2023-01-15 15:46:07 -0600

    find the city added to js

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,30 @@ var currentHumidEl = document.querySelector("#current-humid");
 var currentWindEl = document.querySelector("#current-wind");
 var forecastContainerParentEl = document.querySelector("#forecast-container");
 var forecastContainerEl = document.querySelector(".forecast-weather");
+
+var getCityCoords = function (city) {
+    var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&APPID=718908fbd7f74c0aac0bd0ff69325a23&units=imperial";
+
+    fetch(apiUrl).then(function (response) {
+        if (response.ok) {
+            response.json().then(function (data) {
+                var latitude = data.coord.lat;
+                var longitude = data.coord.lon;
+
+                getCurrentWeather(data);
+                callForecastApi(latitude, longitude);
+
+                var city = data.name;
+                if (!cities.includes(city)) {
+                    cities.push(city);
+                }
+                saveCities();
+
+                addHistoricalSearch(city);
+
+            })
+        } else {
+            alert("Error: City Not Found. Please enter a valid city name.")
+        }
+    });
+};
