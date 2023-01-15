050b841e2f325feb0af10c11e9ec06fd3b681fb0
gregorywyrick <gregmwyrick@gmail.com>
2023-01-15 15:45:02 -0600

    html and vars made

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+var cities = [];
+
+var searchFormEl = document.querySelector("#search-form");
+var cityInputEl = document.querySelector("#city");
+var searchContainerEl = document.querySelector("#search-container");
+var historicalSearchesEl = document.querySelector("#historical-searches");
+var currentWeatherEl = document.querySelector(".current-weather");
+var cityNameEl = document.querySelector("#city-name");
+var currentDateEl = document.querySelector("#current-date");
+var weatherIconEl = document.querySelector(".weather-icon");
+var currentTempEl = document.querySelector("#current-temp");
+var currentHumidEl = document.querySelector("#current-humid");
+var currentWindEl = document.querySelector("#current-wind");
+var forecastContainerParentEl = document.querySelector("#forecast-container");
+var forecastContainerEl = document.querySelector(".forecast-weather");
